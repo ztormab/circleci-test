7e8705455da464a0ddbcd2fefcdd866161d1770f
Per Persson <md2perpe+github@gmail.com>
2015-10-05 17:32:45 +0200

    Update FirstTest.php

--- tests/FirstTest.php
+++ tests/FirstTest.php
@@ -5,6 +5,8 @@ class FirstTest  extends PHPUnit_Framework_TestCase
   {
     echo "Running a dummy test", PHP_EOL;
     
+    $names = [ 'Adam', 'Bertil', 'Caesar' ];
+    
     $pdo = new \PDO('mysql:dbname=circle_test;host=localhost', 'ubuntu', '');
   }
 }
