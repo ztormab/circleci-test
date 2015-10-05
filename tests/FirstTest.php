7ab0d3a636073008014a1611b0b8b33df4c0f1af
Per Persson <md2perpe+github@gmail.com>
2015-10-05 17:07:40 +0200

    Update FirstTest.php

--- tests/FirstTest.php
+++ tests/FirstTest.php
@@ -4,6 +4,8 @@ class FirstTest  extends PHPUnit_Framework_TestCase
   public function testSomething()
   {
     echo "Running a dummy test", PHP_EOL;
+    
+    $pdo = new \PDO('mysql:dbname=mydb;host=localhost');
   }
 }
 
