223f8b9f09ce34194908ce9fc25286cdaff2d82f
Per Persson <per.persson@exertisztorm.com>
2015-10-07 14:54:09 +0200

    Do only a db connection

--- tests/FirstTest.php
+++ tests/FirstTest.php
@@ -3,10 +3,6 @@ class FirstTest  extends PHPUnit_Framework_TestCase
 {
   public function testSomething()
   {
-    echo "Running a dummy test", PHP_EOL;
-    
-    $names = [ 'Adam', 'Bertil', 'Caesar' ];
-    
     $pdo = new \PDO('mysql:dbname=circle_test;host=localhost', 'ubuntu', '');
   }
 }
